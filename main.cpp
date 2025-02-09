e12b6ae1aa8b4ca6aaaa434c1b0e050533a42e2f
Mohammad Rezaeian <mohammadrezaeian598@gmail.com>
2025-02-09 17:39:22 +0330

    Tree Complete Generate

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include "Tree.h"
+
 int main() {
-    Tree *tree = new tree('A');
-    tree->setMLeft(new tree('B'));
+    Tree *tree = new Tree('A');
+    tree->setMLeft(new Tree('B'));
+    tree->setMRight(new Tree('C'));
+
+    tree->getMRight()->setMLeft(new Tree('G'));
+    tree->getMRight()->setMRight(new Tree('H'));
+
+    tree->getMLeft()->setMLeft(new Tree('D'));
+    tree->getMLeft()->setMRight(new Tree('F'));
+    tree->getMLeft()->setMLeft(new Tree('E'));
+
 
     return 0;
 }
 // TODO: Adding the rest of the leaves and branches
 //  Implementation Preorder Traversal for read leaves and branches
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
